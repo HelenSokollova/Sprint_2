ca39b0691c73e314e61048d20f12da27517aaa46
Елена Соколова <qwezxc.sokolova@yandex.ru>
2025-12-25 16:13:01 +0300

    Исправления task_3 вторая версия

--- task_3.py
+++ task_3.py
@@ -1,34 +1,31 @@
 class PointsForPlace:
-    points = 0
     
     @staticmethod
     def get_points_for_place(place):
+        points = 0
         if place > 100:
-            return 'Баллы начисляются только первым 100 участникам'
+            print('Баллы начисляются только первым 100 участникам')
         elif place < 1:
-            return 'Спортсмен не может занять нулевое или отрицательное место'
+            print('Спортсмен не может занять нулевое или отрицательное место')
         else:
-            PointsForPlace.points = 101 - place
-            return PointsForPlace.points
+            points = 101 - place
+        return points
          
-
 class PointsForMeters:
-    points = 0
 
     @staticmethod
     def get_points_for_meters(meters):
+        points = 0
         if meters < 0:
-            return 'Количество метров не может быть отрицательным'
+            print('Количество метров не может быть отрицательным')
         else:
-            PointsForMeters.points = meters * 0.5
-            return PointsForMeters.points
+            points = meters * 0.5
+        return points
 
 class TotalPoints(PointsForPlace, PointsForMeters):
     
     def get_total_points(self, place, meters):
-        place_points = self.get_points_for_place(place)
-        meters_points = self.get_points_for_meters(meters)
-        total = place_points + meters_points
+        total = self.get_points_for_place(place) + self.get_points_for_meters(meters)
         return total
 
 
